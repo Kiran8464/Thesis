2653e07884387a5c7797e7ee9d5cf7740a4c16a9
A <devulapallysaikiranreddy@gmail.com>
2024-12-10 08:01:45 +0000

    Updated code

---
+++ Terraform-aws/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.77.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-re-backend-s3-sai"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
